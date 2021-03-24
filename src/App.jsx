8caa80862e62e8861cc9edbab5326c643facbb95
fabiancrc124 <71909548+fabiancrc124@users.noreply.github.com>
2021-03-24 18:39:04 -0500

    Interfaz calculadora

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+  
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button'
+
+export default function SimpleContainer() {
+  return (
+    <React.Fragment>
+      <Button variant="contained" color="default">
+          
+          </Button>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+        
+      </Container>
+    </React.Fragment>
+  );
+}
