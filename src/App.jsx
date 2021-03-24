321364b341bf03623408df6d41ea3ae850fe5653
fabiancrc124 <71909548+fabiancrc124@users.noreply.github.com>
2021-03-24 18:47:07 -0500

    Cambios en la interfaz

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button'
 
+
 export default function SimpleContainer() {
   return (
     <React.Fragment>
       <Button variant="contained" color="default">
-          
+          <Container maxWidth="xs">
+            
+          </Container>
           </Button>
       <CssBaseline />
       <Container maxWidth="sm">
